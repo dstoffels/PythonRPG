9e807fb7eb4979fc1ce78e42cd982bbf80ca11c9
dstoffels <dan.stoffels@gmail.com>
2021-12-20 16:30:49 -0600

    added displayMainMenu function for game start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from contextlib import ExitStack
 import threading
+from constants import MAIN_MENU
 import threading
 
 gameActive = True #global
@@ -15,4 +16,19 @@ def runInputThread():
 
   threading.Thread(target= userInput).start()
 
-runInputThread()
+def displayMainMenu():
+  userInput = input(MAIN_MENU)
+
+  match userInput:
+    case 1:
+      runInputThread()
+    case 2:
+      print('PLACEHOLDER: game instructions here') #FIXME: need game instructions
+    case 3:
+      print('Until next time, hero')
+      exit()
+    case _:
+      print('PLACEHOLDER: ERROR! ERROR! ERROR!') #FIXME: need input validation
+
+
+
