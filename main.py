2f9f4cdef3f24a337f45e1cd3a3e6ce76bc6f228
dstoffels <dan.stoffels@gmail.com>
2021-12-20 16:14:36 -0600

    added runInputThread function to main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-mainMenu = '''Welcome Hercules!
+from contextlib import ExitStack
+import threading
+import threading
+
+MAIN_MENU = '''Welcome Hercules!
 1) New Game
-2) Exit
+2) How To Play
+3) Exit
 '''
 
-userInput = ''
+gameActive = True #global
 
-def RunGame():
-  userInput = input(mainMenu)
-  print(userInput)
+def runInputThread():
+  def userInput():
+    global gameActive
 
-RunGame()
+    while gameActive:
+      i = input('> ')
+      if(i == 'q'):
+        gameActive = False
+
+  threading.Thread(target= userInput).start()
+
+runInputThread()
