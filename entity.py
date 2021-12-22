082d6078ee792f2e62a5fc468d80d76f9edac95f
dstoffels <dan.stoffels@gmail.com>
2021-12-22 08:35:34 -0600

    UPDATE: added movement funcs to player class, added player object to gameState class

--- entity.py
+++ entity.py
@@ -1,20 +1,58 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from locations.location import location
+from locations.locations import LOCATIONS
 
 class Entity(ABC):
   name = ''
-  currentLocation = (0,0)
-  maxHP = 100
+  currentLocation: location = None
+  maxHP = 0
   currentHP = 0
 
-  def __init__(self, name, currentLocation):
+  def __init__(self, name='', currentLocation: location = location(), maxHp = 100):
     self.name = name
     self.currentLocation = currentLocation
+    self.maxHP = maxHp
     self.currentHP = self.maxHP
+  
+  @abstractmethod
+  def attack(self):
+    pass
 
 class Baddie(Entity):
-  def randomAttack():
-    return #FIXME: need list of different attacks, needs variable attack timer that attacks player only when player is in same location
+  def __init__(self, name='', currentLocation=(0, 0), maxHp=100):
+      super().__init__(name=name, currentLocation=currentLocation, maxHp=maxHp)
+
+  def attack(self): 
+    print('baddie attacks') #FIXME: need list of different attacks, needs variable attack timer that attacks player only when player is in same location
 
 class Player(Entity):
-  def Attack(attackType):
-    return #FIXME: 
+  def attack(self, attackType=''):
+    print('player attacks')
+
+  def moveNorth(self):
+    newCoords =  self.changeCoordinates(-1)
+    self.currentLocation = LOCATIONS[newCoords]
+    self.currentLocation.displayDescription(LOCATIONS)
+
+  def moveSouth(self):
+    newCoords =  self.changeCoordinates(1)
+    self.currentLocation = LOCATIONS[newCoords]
+    self.currentLocation.displayDescription(LOCATIONS)
+
+  def moveEast(self):
+    newCoords =  self.changeCoordinates(0, 1)
+    self.currentLocation = LOCATIONS[newCoords]
+    self.currentLocation.displayDescription(LOCATIONS)
+    1
+  def moveWest(self):
+    newCoords =  self.changeCoordinates(0, -1)
+    self.currentLocation = LOCATIONS[newCoords]
+    self.currentLocation.displayDescription(LOCATIONS)
+
+  def changeCoordinates(self, row=0, col=0):
+    coords = self.currentLocation.coords
+    row = coords[0] + row
+    col = coords[1] + col
+    return (row, col)
+
+
--- gameState.py
+++ gameState.py
@@ -1,5 +1,11 @@
+from entity import Player
+from locations.locations import LOCATIONS
+
+STARTING_LOCATION = (1,6)
+
 class GameState:
   isActive = False
+  player = Player('Hercules', LOCATIONS[STARTING_LOCATION])
 
   def __init__(self):
     self.isActive = True
